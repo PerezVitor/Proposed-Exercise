eee8d65d57b0e41dc57e4619286ffda0f571d744
Vitor Perez <vitor.g.perez@hotmail.com>
2018-12-19 19:15:53 -0200

    Included new Product class

--- Proposed_Exercise/Entities/Client.cs
+++ Proposed_Exercise/Entities/Client.cs
@@ -8,5 +8,28 @@ namespace Proposed_Exercise.Entities
 {
     class Client
     {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        { }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return "Client "
+                + Name
+                + " "
+                + BirthDate
+                + " - "
+                + Email;
+        }
     }
 }
---
+++ Proposed_Exercise/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proposed_Exercise.Entities
+{
+    class Product
+    {
+        public String Name { get; set; }
+        public Double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Price;
+        }
+    }
+}
