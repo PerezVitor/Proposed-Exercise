42505baaa3d157671e60b9a77c2bddbf03b1ee01
Vitor Perez <vitor.g.perez@hotmail.com>
2018-12-19 20:02:07 -0200

    Completed classes inclusion

--- Proposed_Exercise/Entities/Order.cs
+++ Proposed_Exercise/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proposed_Exercise.Entities.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,44 @@ namespace Proposed_Exercise.Entities
     class Order
     {
         public DateTime Moment { get; set; }
-        public OrderStatus order { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order()
+        { }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.subTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order Moment: " + Moment);
+            sb.AppendLine("Order Status: " + Status);
+            sb.AppendLine(Client.ToString());
+            sb.AppendLine("Order Items");
+            foreach(OrderItem item in Items)
+            {
+                sb.AppendLine(Items.ToString());
+            }
+            sb.Append("Total price: " + Total().ToString("c"));
+
+            return sb.ToString();
+        }
     }
 }
